811a02b7cd66b60ced05f01e382fa6d948ebd0a7
Stephane <stephane@lephare-ildere.com>
2026-02-17 06:01:42 +0100

    From: adresse propriétaire Resend

--- mail.py
+++ mail.py
@@ -7,7 +7,7 @@ resend.api_key = "re_MK4pWNHu_3176dxmyuYA77kQDFz78Z6tY"
 def send_welcome_email(user, password):
     try:
         resend.Emails.send({
-            "from": "Le Phare Check <onboarding@resend.dev>",  # ADRESSE OBLIGATOIRE
+            "from": "stephane@lephare-iledere.com",
             "to": [user.email],
             "subject": "Bienvenue sur Le Phare Check - Vos identifiants",
             "html": f"""
@@ -63,7 +63,7 @@ def send_welcome_email(user, password):
 def send_assignment_email(incident, technicien):
     try:
         resend.Emails.send({
-            "from": "Le Phare Check <onboarding@resend.dev>",  # MÊME ADRESSE ICI
+            "from": "stephane@lephare-iledere.com",
             "to": [technicien.email],
             "subject": f"Incident assigné - {incident.hebergement.emplacement}",
             "html": f"""
